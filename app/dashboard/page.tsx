ec781430d824b80e4a65b6ca101c7a8c1a9cc90b
aungchanpyae33 <khantlu24@gmail.com>
2026-01-07 22:43:32 +0630

    fix: again -again

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,8 @@ export default function DashboardPage() {
   return (
     <div>
       Dashboard page - fix -again
-      <h1>this is h1</h1>
+      <h1>this is h1 </h1>
+      <p>this isp </p>
     </div>
   );
 }
