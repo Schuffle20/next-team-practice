875b4bca238cd01eabc368bea395a1c6d68f1f21
aungchanpyae33 <khantlu24@gmail.com>
2026-01-11 01:20:39 +0630

    fix: modify (user) layout.tsx
    to make another nested layout in profile make easier , move sidebar trigger ui and label inside (user)layout

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,12 +1,6 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
-
 export default function Page() {
   return (
-    <div className=" p-5 w-full h-full  flex flex-col gap-3">
-      <div className=" w-full  flex items-center   border-b border-border">
-        <SidebarTrigger />
-        <h3>Dashboard</h3>
-      </div>
+    <div className=" w-full h-full  flex flex-col gap-3">
       <div className="flex  flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3 text-2xl">
           <div className=" flex items-center justify-center bg-primary aspect-video rounded-xl">
